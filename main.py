5bb2bb12a545dcc58d71e9289541ea2101df97c1
tom <wyrwa.t@gmail.com>
2023-11-13 17:44:27 +0100

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
-user_input = input("Enter your name: ").upper()
 nato_alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet_dic = {row["letter"]: row["code"] for (index, row) in nato_alphabet_df.iterrows()}
 
-phonetic_code = [nato_alphabet_dic[letter] for letter in user_input]
 
-print(phonetic_code)
+def generate_phonetic():
+    user_input = input("Enter your name: ").upper()
+    try:
+        phonetic_code = [nato_alphabet_dic[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
+
+
+generate_phonetic()
